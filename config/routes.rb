f49ba3f3d448442f35655d409790c91bd93a4957
Benjamin Miles <dootbin@gmail.com>
2018-04-10 15:36:35 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+ root controller: 'home', action: 'index'
 
-  get 'home/about'
+ get "/about", controller: 'home', action: 'about', as: 'about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
