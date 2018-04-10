5eb3b109384506e0ab2e2f810dfb39ab29233aa5
Benjamin Miles <dootbin@gmail.com>
2018-04-10 16:13:08 -0500

    added routes for contact page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
  get "/about", controller: 'home', action: 'about', as: 'about'
 
+ get "/teams", controller: 'home', action: 'teams', as: 'teams'
+
+ get "/contact", controller: 'home', action: 'contact', as: 'contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
